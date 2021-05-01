6982605a51dede036377a5e22b7eef2cdd352925
Aditya Vij <adityavij@MacBook-Pro.local>
2021-05-01 14:26:57 -0700

    first commit

---
+++ practice/practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  practice
+//
+//  Created by Aditya Vij on 5/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("My App")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ practice/practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Aditya Vij on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
